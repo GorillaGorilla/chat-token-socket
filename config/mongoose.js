ba0548f53779a7c38f26e353092d3ce78a9d7e7f
Frederick Macgregor <frederickmacgregor@fredericks-mbp.sbank.uk.ibm.com>
2016-09-21 10:18:04 +0100

    Separate route functions

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 var config = require('./config'),
     mongoose = require('mongoose');
 module.exports = function() {
+    console.log("connection");
     var db = mongoose.connect(config.db);
     require('../app/models/user.server.model');
     return db;
--- server.js
+++ server.js
@@ -7,13 +7,14 @@ var express     = require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
-var db    = require('./config/mongoose.js');
+var db          = require('./config/mongoose.js')();
 var passport	= require('passport');
 var config      = require('./config/database'); // get db config file
 var User        = require('./app/models/user.server.model'); // get the mongoose model
 var port        = process.env.PORT || 8080;
 var jwt         = require('jwt-simple'),
-    cfenv = require('cfenv');
+    cfenv = require('cfenv'),
+    controller = require("./app/routes/auth.server.controller.js");
 
 // get our request parameters
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -38,82 +39,15 @@ require('./config/passport')(passport);
 var apiRoutes = express.Router();
 
 // create a new user account (POST http://localhost:8080/api/signup)
-apiRoutes.post('/signup', function(req, res) {
-    if (!req.body.name || !req.body.password) {
-        res.json({success: false, msg: 'Please pass name and password.'});
-    } else {
-        var newUser = new User({
-            name: req.body.name,
-            password: req.body.password
-        });
-        // save the user
-        newUser.save(function(err) {
-            if (err) {
-                return res.json({success: false, msg: 'Username already exists.'});
-            }
-            res.json({success: true, msg: 'Successful created new user.'});
-        });
-    }
-});
+apiRoutes.post('/signup', controller.signup);
 
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
-apiRoutes.post('/authenticate', function(req, res) {
-    User.findOne({
-        name: req.body.name
-    }, function(err, user) {
-        if (err) throw err;
-
-        if (!user) {
-            res.send({success: false, msg: 'Authentication failed. User not found.'});
-        } else {
-            // check if password matches
-            user.comparePassword(req.body.password, function (err, isMatch) {
-                if (isMatch && !err) {
-                    // if user is found and password is right create a token
-                    var token = jwt.encode(user, config.secret);
-                    // return the information including token as JSON
-                    res.json({success: true, token: 'JWT ' + token});
-                } else {
-                    res.send({success: false, msg: 'Authentication failed. Wrong password.'});
-                }
-            });
-        }
-    });
-});
+apiRoutes.post('/authenticate', controller.authenticate);
 
 // route to a restricted info (GET http://localhost:8080/api/memberinfo)
-apiRoutes.get('/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
-    var token = getToken(req.headers);
-    if (token) {
-        var decoded = jwt.decode(token, config.secret);
-        User.findOne({
-            name: decoded.name
-        }, function(err, user) {
-            if (err) throw err;
+apiRoutes.get('/memberinfo', passport.authenticate('jwt', { session: false}), controller.memberInfo);
 
-            if (!user) {
-                return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
-            } else {
-                res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
-            }
-        });
-    } else {
-        return res.status(403).send({success: false, msg: 'No token provided.'});
-    }
-});
 
-getToken = function (headers) {
-    if (headers && headers.authorization) {
-        var parted = headers.authorization.split(' ');
-        if (parted.length === 2) {
-            return parted[1];
-        } else {
-            return null;
-        }
-    } else {
-        return null;
-    }
-};
 
 // connect the api routes under /api/*
 app.use('/api', apiRoutes);
