6a355ea5e459e423c3dc4ad64d22cfa432495abc
Freddie MacGregor <fm8pref9@gmail.com>
2016-11-05 10:23:16 +0000

    add matter demo

--- config/socket_config.js
+++ config/socket_config.js
@@ -3,13 +3,54 @@
  */
 "use strict";
 
+var Matter = require('matter-js');
+
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    running = false;
+
+// create an engine
+var engine = Engine.create();
+
+
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+World.add(engine.world, [boxA, boxB, ground]);
+
+// // run the engine
+// Engine.run(engine);
+//
+// // run the renderer
+// Render.run(render);
+
+
+
 
 var numUsers = 0;
 
 module.exports = function(io){
 
+
+
     io.on('connection', function (socket) {
         var addedUser = false;
+        if (!running){
+            run();
+        }
+
+        function run(){
+            running = true;
+            setInterval(function() {
+                Engine.update(engine, 1000 / 30);
+                socket.emit('gameState',{boxA: boxA.position, boxB: boxB.position});
+            }, 1000 / 2);
+        }
 
         // when the client emits 'new message', this listens and executes
         socket.on('new message', function (data) {
@@ -22,7 +63,7 @@ module.exports = function(io){
 
         // when the client emits 'add user', this listens and executes
         socket.on('add user', function (username) {
-            if (addedUser) return;
+            if (addedUser) {return};
 
             // we store the username in the socket session for this client
             socket.username = username;
@@ -67,4 +108,9 @@ module.exports = function(io){
     });
 
 };
+
+
+
+
+
 
