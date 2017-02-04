f228b629ec526cedef3c768c6c7952138f7c9661
Brian Kane <briankane1@gmail.com>
2017-02-04 18:03:11 +0000

    Now sends emails:

--- server.js
+++ server.js
@@ -102,5 +102,28 @@ try {
         if(err) return console.log("Pretty fucked now, nothing to catch this.. \n", err);
         console.log(error_report);
     });
+
+    var mailer = require('nodemailer');
+    var mailTransport = mailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'youremail@gmail.com',
+            pass: '' //you need to create an app password in gmail for this => https://myaccount.google.com/apppasswords
+        }
+    });
+
+    var mailOptions = {
+        from: 'Server <server@gameapp.com>', // sender address
+        to: 'briankane1@gmail.com', // list of receivers
+        subject: 'Crash Report', // Subject line
+        text: error_report
+    };
+
+    mailTransport.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
 }
 
